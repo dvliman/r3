835acd7ffd734adbc7e8ae659a073aa5816b1854
David Liman <limanoit@gmail.com>
2021-08-24 22:06:37 -0700

    add deps: expo async storage and react navigation

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>And see this changing</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [location, setLocation] = useState(null);
+  const [UIState, setUIState] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setUIState('location-error');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setUIState('location-received');
+    })();
+  }, []);
+
+  console.log(UIState);
+  if (UIState === 'location-error') {
+    return (
+      <View style={styles.container}>
+        <Text>There was some error getting location</Text>
+      </View>
+    )
+  } else if (UIState === 'location-received') {
+    return (
+      <View style={styles.container}>
+        <Text>Latitude</Text>
+        <Text>{location["coords"]["latitude"]}</Text>
+        <Text>Longitude</Text>
+        <Text>{location["coords"]["longitude"]}</Text>
+        <Text>Accuracy</Text>
+        <Text>{location["coords"]["accuracy"]}</Text>
+      </View>
+    );
+  // } else if (UIState === 'saved-locations') {
+  } else {
+    console.log('display saved location');
+    return (
+      <View style={styles.container}>
+        <Text>displaying saved location</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
