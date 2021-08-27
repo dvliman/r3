8527788feae20822f9fc6bec2799aa0f65351979
David Liman <limanoit@gmail.com>
2021-08-26 17:41:02 -0700

    wire up navigation and install native-base

--- App.js
+++ App.js
@@ -1,60 +1,65 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MoreScreen from './MoreScreen';
+import HomeScreen from './HomeScreen';
+import { Home as HomeIcon, FaceVeryHappy } from 'akar-icons';
 
+const Home = createNativeStackNavigator();
+const More = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Home.Navigator screenOptions={{ headerShown: false }}>
+      <Home.Screen name="HomeScreen" component={HomeScreen} />
+    </Home.Navigator>
+  );
+}
+
+function MoreStack() {
+  return (
+    <More.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="MoreScreen" component={MoreScreen} />
+    </More.Navigator>
+  );
+}
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let icon;
+
+          if (route.name === 'Home') {
+            icon = focused ? TabHomeActiveImage : TabHomeImage;
+          } else if (route.name === 'More') {
+            icon = focused ? TabMoreActiveImage : TabMoreImage;
+          }
+          return <HomeIcon/>;
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="More" component={MoreStack} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
-  const [location, setLocation] = useState(null);
-  const [UIState, setUIState] = useState(null);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     let { status } = await Location.requestForegroundPermissionsAsync();
-  //     if (status !== 'granted') {
-  //       setUIState('location-error');
-  //       return;
-  //     }
-  //
-  //     let location = await Location.getCurrentPositionAsync({});
-  //     setLocation(location);
-  //     setUIState('location-received');
-  //   })();
-  // }, []);
-  //
-  // console.log(UIState);
-  // if (UIState === 'location-error') {
-  //   return (
-  //     <View style={styles.container}>
-  //       <Text>There was some error getting location</Text>
-  //     </View>
-  //   )
-  // } else if (UIState === 'location-received') {
-  //   return (
-  //     <View style={styles.container}>
-  //       <Text>Latitude</Text>
-  //       <Text>{location["coords"]["latitude"]}</Text>
-  //       <Text>Longitude</Text>
-  //       <Text>{location["coords"]["longitude"]}</Text>
-  //       <Text>Accuracy</Text>
-  //       <Text>{location["coords"]["accuracy"]}</Text>
-  //     </View>
-  //   );
-  // // } else if (UIState === 'saved-locations') {
-  // } else {
-  //   console.log('display saved location');
-  //   return (
-  //     <View style={styles.container}>
-  //       <Text>displaying saved location</Text>
-  //     </View>
-  //   )
-  // }
-
   const Stack = createNativeStackNavigator();
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="More" component={MoreScreen} />
+      <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      intialRouteName="Tab"
+      headerMode="screen"
+      >
+        <Stack.Screen name="Tab" component={TabNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   )
--- HomeScreen.js
+++ HomeScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function MoreScreen() {
+export default function HomeScreen() {
   return (
     <View>
-      <Text>more</Text>
+      <Text>home</Text>
     </View>
   )
 }
